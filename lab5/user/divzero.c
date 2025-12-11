6e175f4c9d9dfdbf849865788d8eb6efb7d4889a
Zeshun <10266354@gitlab.com>
2025-12-11 09:15:22 +0800

    div0改为内联汇编,130/130

--- lab5/user/divzero.c
+++ lab5/user/divzero.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <ulib.h>
 
-volatile int zero;
+volatile int zero = 0;
 
-int
-main(void) {
-    cprintf("value is %d.\n", 1 / zero);
+int main(void) {
+    int val;
+    // 使用内联汇编强制生成除法指令，防止编译器优化成奇怪的逻辑
+    asm volatile("divw %0, %1, %2" : "=r"(val) : "r"(1), "r"(zero));
+    cprintf("value is %d.\n", val);
     panic("FAIL: T.T\n");
 }
-
